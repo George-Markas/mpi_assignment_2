dc31d2acf9d90d19eb73dfc546cc7dcd0aa51ee0
George-Markas <georgmarkanton@proton.me>
2025-01-07 01:59:32 +0200

    Got min, max and avg working

--- src/main.c
+++ src/main.c
@@ -2,6 +2,8 @@
 #include "myutils.h"
 #include "mpi.h"
 
+#define ROOT_RANK 0 // rank of the main process
+
 int main(int argc, char* argv[]) {
     // Initialize MPI
     MPI_Init(&argc, &argv);
@@ -11,14 +13,13 @@ int main(int argc, char* argv[]) {
     MPI_Comm_rank(MPI_COMM_WORLD, &process_id);
     MPI_Comm_size(MPI_COMM_WORLD, &process_count);
 
-    const int mpi_root = 0; // Main process
+    int length; // Length of the vector
     int n_count; // How many numbers the process is going to receive
     int* receive_counts = calloc(process_count, sizeof(int)); // How many numbers each process will receive via Scatterv
     int* displacements = calloc(process_count, sizeof(int)); // Displacements for Scatterv
     float *vector = NULL;
 
-    if(process_id == mpi_root) { // Start of main process logic
-        int length;
+    if(process_id == ROOT_RANK) {
         fputs("Input the vector's length: ",  stdout);
         fflush(stdout);
         read_int(&length, 1, 0);
@@ -79,23 +80,49 @@ int main(int argc, char* argv[]) {
             printf("P%d | %d\n", i, receive_counts[i]);
         }
         putc('\n', stdout);
-    } // End of main process logic
+    }
 
     // Inform each process how many numbers it is going to receive
-    MPI_Scatter(receive_counts, 1, MPI_INT, &n_count, 1, MPI_INT, mpi_root, MPI_COMM_WORLD );
+    MPI_Scatter(receive_counts, 1, MPI_INT, &n_count, 1, MPI_INT, ROOT_RANK, MPI_COMM_WORLD );
     float* receive_buffer = calloc(n_count, sizeof(float));
 
+    // Send each process its predefined portion of the load
     MPI_Barrier(MPI_COMM_WORLD);
     MPI_Scatterv(vector, receive_counts, displacements, MPI_FLOAT, receive_buffer, n_count,
-        MPI_INT, mpi_root, MPI_COMM_WORLD);
+        MPI_INT, ROOT_RANK, MPI_COMM_WORLD);
 
-    printf("Rank: %d | n_count: %d\n", process_id, n_count);
+    //printf("Rank: %d | n_count: %d\n", process_id, n_count);
     MPI_Barrier(MPI_COMM_WORLD);
 
+    // Calculate partition average
+    float partition_sum = 0;
+    for(int i = 0; i < n_count; i++) {
+        partition_sum += receive_buffer[i];
+    }
+
     // DEBUG
-    // for(int i = 0; i < n_count; i++) {
-    //       printf("%.2f\n", receive_buffer[i]);
-    // }
+    printf("%d) Part sum: %.3f\n", process_id, partition_sum);
+
+    // Gather all the partition sums and sum the up
+    float vector_sum, vector_max, vector_min;
+
+    // Using a temporary copy of receive_buffer to avoid undefined behaviour on subsequent reductions
+    float* temp = calloc(n_count, sizeof(float));
+    memcpy(temp, receive_buffer, n_count * sizeof(float));
+
+    // Get the min, max and the sum of values held by the processes
+    MPI_Reduce(receive_buffer, &vector_min, n_count, MPI_FLOAT, MPI_MIN, ROOT_RANK, MPI_COMM_WORLD);
+    MPI_Reduce(temp, &vector_max, n_count, MPI_FLOAT, MPI_MAX, ROOT_RANK, MPI_COMM_WORLD);
+    MPI_Reduce(&partition_sum, &vector_sum, 1, MPI_FLOAT, MPI_SUM, ROOT_RANK, MPI_COMM_WORLD);
+
+    free(temp);
+    temp = NULL;
+
+    if(process_id == ROOT_RANK) {
+        printf("Min: %.3f\n", vector_min);
+        printf("Max: %.3f\n", vector_max);
+        printf("Average: %.3f\n", vector_sum / (float) length);
+    }
 
     free(receive_counts);
     receive_counts = NULL;
